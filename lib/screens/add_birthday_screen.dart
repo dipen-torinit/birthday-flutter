b9cb9d7f75f62d1b51e7cb3802880486832e092b
shweta <shweta.salunkhe@torinit.ca>
2022-01-19 16:24:27 +0530

    Added scrollview

--- lib/screens/add_birthday_screen.dart
+++ lib/screens/add_birthday_screen.dart
@@ -43,83 +43,85 @@ class _AddBirthdayScreenState extends State<AddBirthdayScreen> {
           progressState = "Add birthday detail";
         }
         return Scaffold(
-          body: Container(
-            padding: const EdgeInsets.all(20),
-            color: Colors.cyan,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(progressState),
-                  const Icon(
-                    Icons.person,
-                    size: 100,
-                  ),
-                  TextFormField(
-                    controller: nameController,
-                    validator: (value) {
-                      if (!Validation.isNameValid(value.toString())) {
-                        return 'Please enter name';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                        hintText: "Enter name", labelText: "Name"),
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    validator: (value) {
-                      if (!Validation.isEmailValid(value.toString())) {
-                        return 'Please enter email';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                        hintText: "Enter email", labelText: "Email"),
-                  ),
-                  TextFormField(
-                    controller: phoneController,
-                    validator: (value) {
-                      if (!Validation.isPhoneValid(value.toString())) {
-                        return 'Please enter phone number';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                        hintText: "Enter phone number",
-                        labelText: "Phone Number"),
-                  ),
-                  TextFormField(
-                    controller: dateController,
-                    validator: (value) {
-                      if (!Validation.isValidDate(value.toString())) {
-                        return 'Please enter birthdate';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                        hintText: "Enter birthdate(DD/MM/YYYY)",
-                        labelText: "Birthdate"),
-                  ),
-                  TextButton(
-                      child: const Text(
-                        'Add Birthday',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          Provider.of<BirthdayList>(context, listen: false)
-                              .addBirthday(
-                                  "",
-                                  nameController.text,
-                                  emailController.text,
-                                  phoneController.text,
-                                  dateController.text);
+          body: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              color: Colors.cyan,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(progressState),
+                    const Icon(
+                      Icons.person,
+                      size: 100,
+                    ),
+                    TextFormField(
+                      controller: nameController,
+                      validator: (value) {
+                        if (!Validation.isNameValid(value.toString())) {
+                          return 'Please enter name';
                         }
-                      }),
-                ],
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "Enter name", labelText: "Name"),
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (!Validation.isEmailValid(value.toString())) {
+                          return 'Please enter email';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "Enter email", labelText: "Email"),
+                    ),
+                    TextFormField(
+                      controller: phoneController,
+                      validator: (value) {
+                        if (!Validation.isPhoneValid(value.toString())) {
+                          return 'Please enter phone number';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "Enter phone number",
+                          labelText: "Phone Number"),
+                    ),
+                    TextFormField(
+                      controller: dateController,
+                      validator: (value) {
+                        if (!Validation.isValidDate(value.toString())) {
+                          return 'Please enter birthdate';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "Enter birthdate(DD/MM/YYYY)",
+                          labelText: "Birthdate"),
+                    ),
+                    TextButton(
+                        child: const Text(
+                          'Add Birthday',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            Provider.of<BirthdayList>(context, listen: false)
+                                .addBirthday(
+                                    "",
+                                    nameController.text,
+                                    emailController.text,
+                                    phoneController.text,
+                                    dateController.text);
+                          }
+                        }),
+                  ],
+                ),
               ),
             ),
           ),
